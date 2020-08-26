f8f69f1f374646f4b85822b75b89c4c6fecbe433
KohnoTaiyo <taiyowork66@gmail.com>
2020-08-26 13:17:28 +0900

    init interface

---
+++ src/interface.ts
@@ -0,0 +1,22 @@
+interface Human {
+  name: string;
+  age: number;
+  greeting(message: string): void;
+}
+
+class Developer implements Human {
+  constructor(public name: string, public age: number, public experience: number) { }
+  greeting(message: string) {
+    console.log(`Hello! ${message}`)
+  }
+}
+const tmpDeveloper = {
+  name: 'Quill',
+  age: 45,
+  experience: 10,
+  greeting(message: string) {
+    console.log(`Hello! ${message}`)
+  }
+}
+// const user = new Developer('Quill', 45, 10);
+const user: Human = tmpDeveloper;
